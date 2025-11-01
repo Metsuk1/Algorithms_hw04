b1c742d87c167cc73e814d8730d522bab20f6c67
Abayev <240918@astanait.edu.kz>
2025-11-01 12:10:09 +0500

    feature(models): provided all entities

--- src/main/java/graph/models/Graph.java
+++ src/main/java/graph/models/Graph.java
@@ -1,4 +1,41 @@
 package graph.models;
 
+import java.util.*;
+
 public class Graph {
+    private final int nodeCount;
+    private final Map<Vertex, List<Edge>> outgoing;
+    private final Map<Vertex, List<Edge>> incoming;
+
+    public Graph(int nodeCount) {
+        this.nodeCount = nodeCount;
+        this.outgoing = new HashMap<>();
+        this.incoming = new HashMap<>();
+        for (int i = 0; i < nodeCount; i++) {
+            Vertex node = new Vertex(i);
+            outgoing.put(node, new ArrayList<>());
+            incoming.put(node, new ArrayList<>());
+        }
+    }
+
+    public void addEdge(Vertex from, Vertex to, int weight) {
+        Edge edge = new Edge(from, to, weight);
+        outgoing.get(from).add(edge);
+        incoming.get(to).add(edge);
+    }
+
+    public List<Edge> getOutgoing(Vertex node) {
+        return Collections.unmodifiableList(outgoing.get(node));
+    }
+
+    public List<Edge> getIncoming(Vertex node) {
+        return Collections.unmodifiableList(incoming.get(node));
+    }
+
+    public Collection<Vertex> getNodes() {
+        return Collections.unmodifiableSet(outgoing.keySet());
+    }
+
+    public int getVertexCount() { return nodeCount; }
+    public Vertex getVertex(int id) { return new Vertex(id); }
 }
--- src/main/java/graph/models/Vertex.java
+++ src/main/java/graph/models/Vertex.java
@@ -1,4 +1,19 @@
 package graph.models;
 
 public class Vertex {
+    private final int id;
+
+    public Vertex(int id) { this.id = id; }
+
+    public int getId() { return id; }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Vertex v && v.id == id;
+    }
+
+    @Override
+    public int hashCode() { return Integer.hashCode(id); }
+    @Override
+    public String toString() { return "T" + id; }
 }
